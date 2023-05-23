ac4bb7b0a7b3e4d1dc6c628bfc29199c3548c981
suleman <sulemmanbhatti360@gmail.com>
2023-05-23 23:43:14 +0500

    code pushed to Suleman branch from master branch

--- tau-intro-to-pytest/tests/test_math.py
+++ tau-intro-to-pytest/tests/test_math.py
@@ -1,3 +1,5 @@
+import pytest
+
 def test_one_plus_one():
     assert 1+1==2
 
@@ -5,4 +7,9 @@ def test_one_plus_two():
     assert 1+2==0
 
 def test_one_plus_three():
-        assert 1 + 3 == 4
+    assert 1 + 3 == 4
+
+def division_by_zero():
+    with pytest.raises(ZeroDivisionError) as e:
+        num=1/0
+    assert 'Division by zero' in str(e.value)
